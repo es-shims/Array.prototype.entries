03b78b363d1984a013ad630ceb78df216cd87015
Jordan Harband <ljharb@gmail.com>
2021-09-30 22:20:34 -0700

    [Deps] update `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var CreateArrayIterator = require('es-create-array-iterator');
-var ToObject = require('es-abstract/2020/ToObject');
+var ToObject = require('es-abstract/2021/ToObject');
 
 module.exports = function entries() {
 	var O = ToObject(this);
