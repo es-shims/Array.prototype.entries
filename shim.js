eba3bbe5d2e9bf3501f647357a7bf7cec2c4165c
Jordan Harband <ljharb@gmail.com>
2022-04-11 15:53:31 -0700

    [New] `shim`/`auto`: add `entries` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayEntries() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ entries: polyfill },
 		{ entries: function () { return Array.prototype.entries !== polyfill; } }
 	);
+
+	shimUnscopables('entries');
+
 	return polyfill;
 };
