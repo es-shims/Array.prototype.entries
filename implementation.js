ea1728cdcdd9fb0c14457df6afad39a5747d1cdb
Jordan Harband <ljharb@gmail.com>
2023-08-28 21:49:58 -0700

    [Deps] update `define-properties`, `es-abstract`, `es-create-array-iterator`

--- implementation.js
+++ implementation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var CreateArrayIterator = require('es-create-array-iterator');
-var ToObject = require('es-abstract/2022/ToObject');
+var ToObject = require('es-abstract/2023/ToObject');
 
 module.exports = function entries() {
 	var O = ToObject(this);
